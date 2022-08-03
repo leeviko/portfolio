f13d592d80c45c7a8ecb63d35eb122b44dec3084
leeviko <leevikotilaine@gmail.com>
2022-08-03 09:00:28 +0300

    Fix build

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 
 import "./App.css";
 import Nav from "./components/Nav";
