73dbb01fb3b29fd4c5163968023bd4ccec69b738
leeviko <leevikotilaine@gmail.com>
2022-09-22 13:11:40 +0300

    Dont change title on about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,7 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 
 const About = () => {
-  useEffect(() => {
-    document.title = "Leevi - Minusta";
-  }, []);
-
   return (
     <div className="about page">
       <div className="page-container">
