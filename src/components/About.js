79338f9a1d03b9d0e31998001be65312f04b8ab7
leeviko <leevikotilaine@gmail.com>
2022-09-22 13:13:29 +0300

    Specify title on about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const About = () => {
+  useEffect(() => {
+    document.title = "Leevi Kotilainen";
+  }, []);
+
   return (
     <div className="about page">
       <div className="page-container">
