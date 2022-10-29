900444e9e1149b90b19a3efa0a1c223982bad883
leeviko <lerppana0009@gmail.com>
2022-10-29 20:08:10 +0300

    Fix fonts

--- components/Layout.js
+++ components/Layout.js
@@ -3,16 +3,22 @@ import Footer from "./Footer";
 
 import { Nunito, Poppins, Source_Code_Pro } from "@next/font/google";
 
-const nunito = Nunito({ subsets: ["latin"] });
+const nunito = Nunito({ subsets: ["latin"], variable: "--nunito" });
 const poppins = Poppins({
   weight: "400",
   subsets: ["latin"],
+  variable: "--poppins",
 });
-const sourceCodePro = Source_Code_Pro({ subsets: ["latin"] });
+const sourceCodePro = Source_Code_Pro({
+  subsets: ["latin"],
+  variable: "--sourceCodePro",
+});
 
 export default function Layout({ children }) {
   return (
-    <div className={`container ${nunito} ${poppins} ${sourceCodePro}`}>
+    <div
+      className={`container ${nunito.variable} ${poppins.variable} ${sourceCodePro.variable}`}
+    >
       <div className="wrapper">
         <Nav />
         <main className="page">
